602ea698c53bc6efb1c768d7e6d663fea23a197e
esh4 <eshel.sinclair@gmail.com>
2018-12-24 17:30:35 +0200

    Fix OI

--- TrigonGithubHanuka2018/src/org/usfirst/frc/team5990/robot/OI.java
+++ TrigonGithubHanuka2018/src/org/usfirst/frc/team5990/robot/OI.java
@@ -9,25 +9,29 @@ package org.usfirst.frc.team5990.robot;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.XboxController;
-
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	XboxController driveStick = new XboxController(0);
+	XboxController operatorXbox = new XboxController(0);
+	Button AButton, XButton, YButton, BButton;
 
 	public OI() {
+		AButton = new JoystickButton(operatorXbox, 1);
+		XButton = new JoystickButton(operatorXbox, 3);
+		YButton = new JoystickButton(operatorXbox, 4);
+		BButton = new JoystickButton(operatorXbox, 2);
 	}
-	
 
-	public double getXboxX(Hand hand){
-		return driveStick.getX(hand);
+	public double getXboxX(Hand hand) {
+		return operatorXbox.getX(hand);
 	}
-	
-	public double getXboxY(Hand hand){
-		return driveStick.getY(hand);
+
+	public double getXboxY(Hand hand) {
+		return operatorXbox.getY(hand);
 	}
 }
-
